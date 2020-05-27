55d8ba9d0b743e9247955a0c9e1699e2f95c10e8
anshubeniwal <beniwalanshum1998@gmail.com>
2020-05-27 23:45:09 +0530

    Angular Assignment 4

---
+++ Assignment 4/src/app/department-detail/department-detail.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router'
+
+@Component({
+  selector: 'app-department-detail',
+  template: `
+    <h3> You selected department with id = {{departmentId}}</h3>
+      <a (click) = "goPrevious()">Previous</a>
+      <a (click) = "goNext()">Next</a>
+    <div>
+      <button (click) = "gotoDepartment()">Back</button>
+    </div>
+    
+  `,
+  styles: [
+  ]
+})
+export class DepartmentDetailComponent implements OnInit {
+
+  public departmentId;
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+
+    // let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    // this.departmentId = id;
+
+    this.route.paramMap.subscribe((params: ParamMap) => {
+
+      let id = parseInt(params.get('id'));
+      this.departmentId = id;
+
+    });
+    
+    }
+
+    goPrevious() {
+      let previousId = this.departmentId - 1;
+      this.router.navigate(['departments', previousId]);
+    }
+
+    goNext() {
+      let nextId = this.departmentId + 1;
+      this.router.navigate(['departments', nextId]);
+    }
+
+    gotoDepartment() {
+      let selected = this.departmentId ? this.departmentId: null;
+      this.router.navigate(['departments', { id: selected}]);
+    }
+  }
